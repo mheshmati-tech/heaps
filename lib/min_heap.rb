10f4abb326e518fdc1d571d7b97e9c2561ba774a
Maryam Heshmati <mairheshmati@gmail.com>
2020-09-28 09:31:28 -0700

    implemented min-heap methods

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -8,7 +8,6 @@ class HeapNode
 end
 
 class MinHeap
-
   def initialize
     @store = []
   end
@@ -17,7 +16,9 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+
+    heap_up(@store.length - 1)
   end
 
   # This method removes and returns an element from the heap
@@ -25,9 +26,18 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
-  end
+    if @store.empty?
+      return nil 
+    end
+    
+    #removing the root element 
+    swap(0, @store.length - 1)
+    result = @store.pop
 
+    #re-arranging heap 
+    heap_down(0) unless @store.empty?
+    return result.value 
+  end
 
   # Used for Testing
   def to_s
@@ -39,7 +49,7 @@ class MinHeap
     end
 
     output += @store.last.value + "]"
-      
+
     return output
   end
 
@@ -47,7 +57,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    @store.empty?
   end
 
   private
@@ -58,14 +68,35 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+    parent = (index - 1) / 2
+    return if index == 0
+
+    if @store[parent].key > @store[index].key
+      swap(parent, index)
+      heap_up(parent)
+    else #@store[parent].key < @store[index].key
+      return
+    end
   end
 
-  # This helper method takes an index and 
+  # This helper method takes an index and
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    right = 2 * index + 2
+    left = 2 * index + 1
+    return if @store[right].nil? || @store[left].nil?
+
+    #determine which is smaller 
+    smaller = @store[left].key > @store[right].key ? right : left
+    
+    #swap 
+    if @store[smaller].key < @store[index].key
+      swap(smaller, index)
+      heap_down(smaller)
+    else
+      return 
+    end
   end
 
   # If you want a swap method... you're welcome
